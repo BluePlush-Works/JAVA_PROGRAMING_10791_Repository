3229ce2aaee652f35fcf01eef7d4c4107d24d7c6
Blue Plushtrap <comicbluebonnie@outlook.com>
2026-06-22 17:56:21 +0100

    Update controler.java

--- src/main/java/com/eifp/__Clinica/controler.java
+++ src/main/java/com/eifp/__Clinica/controler.java
@@ -1,27 +1,89 @@
-package com.iefp._Clinica.controler;
+package com.iefp.clinicaMedica.controller;
 
-inport com.iefp._Clinica.model.Paciente;
-inport org.springframework.stereotype.Controller;
-	
-inport java.util.ArrayList;
-inport java.util.List;	
+import service;
+import org.springframework.stereotype.Controller;
 
-	@Controller
-	public class PacienteControler{
-		private List<Paciente> pacientes = new ArrayList<>();
-		
-		@GetMapping('/pacientes')
-		public String listaPacientes(Model model){
-			model.addAttribute("lista", pacientes);
-			return "/pacientes";
-		}
-		
-		@PostMapping('/pacientes')
-		public String addicionarPaciente(@RequestParan String nome, @RequestParan Integer idade){
-			
-			Paciente paciente = new Paciente(nome, idade);
-			paciente.add(paciente);
-			
-			return "redirect:/pacientes";
-		}
+@Controller
+public class RegistoContoller {
+
+	private final RegistoService registoService;
+
+	public RegistoContoller(RegistoService registoService) {
+		this.registoService = registoService;
 	}
+	
+	@GetMapping("/registar/paciente")
+	public String formularioPaciente(){
+		return "registar-paciente";
+	}
+
+	@PostMapping("/registar/paciente")
+	public String registarPaciente(@RequestParam String nome,@RequestParam String email, @RequestParam String senha, @RequestParam LocalDate dataNascimento, @RequestParam String telefone, @RequestParam String endereco){
+
+		registoService.registarPaciente(nome, email, senha, dataNascimento, telefone, endereco);
+
+		return "redirect:/pacientes";
+	}
+
+	@GetMapping("/registar/Medico")
+	public String formularioPaciente(){
+		return "registar-Medico";
+	}
+
+	@PostMapping("/registar/Medico")
+	public String registarMedico(@RequestParam String nome,
+		@RequestParam String email,	@RequestParam String senha,	@RequestParam LocalDate dataNascimento,	@RequestParam String telefone,	@RequestParam String endereco, @RequestParam String especiblidade){
+
+		registoService.registarMedico(nome, email, senha, dataNascimento, telefone, endereco, especiblidade);
+
+		return "redirect:/Medico";
+	}
+
+	@GetMapping("/registar/Secretaria")
+	public String formularioPaciente(){
+		return "registar-Secretaria";
+	}
+
+	@PostMapping("/registar/Secretaria")
+	public String registarPaciente(@RequestParam String nome,
+		@RequestParam String email,	@RequestParam String senha,	@RequestParam LocalDate dataNascimento,	@RequestParam String telefone,	@RequestParam String endereco){
+
+		registoService.registarPaciente(nome, email, senha, dataNascimento, telefone, endereco);
+
+		return "redirect:/Secretaria";
+	}
+}
+
+
+@Controller
+public class ListagemController {
+	private final ListagemService listagemService;
+
+	public ListagemController(ListagemService listagemService) {
+		this.listagemService = listagemService;
+	}
+	
+	@GetMapping('/utilizadors')
+	public String listarUtilizadors(Model nodel){
+		model.addAtribute("lista", ListagemService.listarUtilizadors());
+		return "utilizadors"
+	}
+	
+	@GetMapping('/paciente')
+	public String listarPaciente(Model nodel){
+		model.addAtribute("lista", ListagemService.listarPaciente());
+		return "paciente"
+	}
+	
+	@GetMapping('/medico')
+	public String listarUtilizadors(Model nodel){
+		model.addAtribute("lista", ListagemService.listarMedico());
+		return "medico"
+	}
+	
+	@GetMapping('/secretaria')
+	public String listarUtilizadors(Model nodel){
+		model.addAtribute("lista", ListagemService.listarSecretaria());
+		return "secretaria"
+	}
+}
