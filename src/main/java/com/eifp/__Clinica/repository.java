3f938b9a18c0d2832c67866d042df81f02b26e46
Blue Plushtrap <comicbluebonnie@outlook.com>
2026-06-22 17:57:26 +0100

    Create repository.java

---
+++ src/main/java/com/eifp/__Clinica/repository.java
@@ -0,0 +1,16 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.iefd._Clinica.model.java;
+
+public interface UtilizadorRepository extends JpaRepository<Utilizador, Long>{
+	aaaaaaaaa
+}
+
+public interface SecretariaRepository extends JpaRepository<Secretaria, Long>{
+	aaaaaaaaa
+}
+
+public interface MedicoRepository extends JpaRepository<Medico, Long>{
+	aaaaaaaaa
+}
