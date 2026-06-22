08e8b94c4dbdf0ebd7e8d4e0785331a18401e250
Blue Plushtrap <comicbluebonnie@outlook.com>
2026-06-22 17:56:41 +0100

    Update model.java

--- src/main/java/com/eifp/__Clinica/model.java
+++ src/main/java/com/eifp/__Clinica/model.java
@@ -1,8 +1,40 @@
 package com.iefp._Clinica.model;
 
-public class Paciente{
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.time.Period;
+
+@Entity
+@Date
+@NoArgsConstructor
+@AllArgsConstructor
+public class Utilizador{
+	
+	@Id
+	@GenerateValue(stratagy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String nome;
-	private Integer idade;
+	
+	@Collum(unique=true)
+	private String email;
+	
+	private String password;
+	private String perfil;
+	private LocalDate dataNashimento;
+	private String telephone;
+	private String endreco;
+	
+	public Integer real_age(){
+		if(dataNashimento==null){
+			return 0;
+		}
+		
+		return Period.between(dataNashimento, LocalDate.now()).getYear();
+	}
 	
 	public Paciente (String nome, Integer idade){
 		this.nome = nome;
@@ -11,4 +43,48 @@ public class Paciente{
 	
 	Public String getnome(){ return nome;}
 	Public Integer getidade(){return idade;}
+}
+
+@Entity
+@Date
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paciente{
+	@Id
+	@GenerateValue(stratagy = GenerationType.IDENTITY)
+	private Long paciente_ID;
+	
+	@OnetoOne
+	@JoinColumn(name = "utilizador_id", unique = true)
+	private Utilizador utilizado;
+}
+
+@Entity
+@Date
+@NoArgsConstructor
+@AllArgsConstructor
+public class Medico{
+	@Id
+	@GenerateValue(stratagy = GenerationType.IDENTITY)
+	private Long medico_ID;
+	
+	private String especiblidade;
+	
+	@OnetoOne
+	@JoinColumn(name = "utilizador_id", unique = true)
+	private Utilizador utilizado;
+}
+
+@Entity
+@Date
+@NoArgsConstructor
+@AllArgsConstructor
+public class Secretaria{
+	@Id
+	@GenerateValue(stratagy = GenerationType.IDENTITY)
+	private Long secretary_ID;
+	
+	@OnetoOne
+	@JoinColumn(name = "utilizador_id", unique = true)
+	private Utilizador utilizado;
 }
