75fefdcbca0e2aac6abc701d737cd741438b2276
Blue Plushtrap <comicbluebonnie@outlook.com>
2026-06-29 15:47:01 +0100

    Update repository.java

--- src/main/java/com/eifp/__Clinica/repository.java
+++ src/main/java/com/eifp/__Clinica/repository.java
@@ -3,6 +3,8 @@ package repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.iefd._Clinica.model.java;
 
+import java.util.List;
+
 public interface UtilizadorRepository extends JpaRepository<Utilizador, Long>{
 	aaaaaaaaa
 }
@@ -12,5 +14,14 @@ public interface SecretariaRepository extends JpaRepository<Secretaria, Long>{
 }
 
 public interface MedicoRepository extends JpaRepository<Medico, Long>{
-	aaaaaaaaa
+	@Query("Select DISTINCT m.especiblidade FROM Medico m WHERE m.especiblidade IS NOT NULL")
+	List<String> listarespeciblidade();
+}
+
+public interface DisponiblidadeRepository extends JpaRepository<Disponiblidade, Long>{
+	List<Disponiblidade> findbyMedico_EspeciblidadeIgnoreCaseAndOcupadaFalseOrderByDataAscHoraInicioAsc(String especiblidade);
+	
+	List<Disponiblidade> findbyMedico_IDOrderByDateAscHoraInicioAsc<Long medico_ID>;
+	
+	Boolean existsByMedico_IDAndDataAndHoraInicioAndHoraFin(Lonf medico_ID, LocalDate date, LocalTime HoraInicio, LocalTime HoraFin);
 }
